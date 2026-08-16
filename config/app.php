ea4f241e3fe72fe4131556b003aa2a2b8a69f63c
shoriful-stack <sisazid182713@gmail.com>
2026-08-16 17:51:14 +0600

    feat: api prefix convention updated

--- config/app.php
+++ config/app.php
@@ -54,6 +54,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:1000'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
--- routes/api.php
+++ routes/api.php
@@ -13,62 +13,62 @@ use App\Http\Controllers\API\V1\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// abc.com/api/v1/oauth/blogs
-Route::group( [
-    'prefix'    => 'v1/oauth',
-    'namespace' => 'API\V1',
-], function () {
+/*
+|--------------------------------------------------------------------------
+| Public CMS API  →  /api/v1/*
+|--------------------------------------------------------------------------
+*/
+Route::prefix('v1')->group(function () {
+    Route::get('/blogs', [BlogController::class, 'index']);
+    Route::get('/blog/{slug}', [BlogController::class, 'show']);
 
-    // Route::get( '/get_branch', [BranchController::class, 'getBranch'] );
-    
-    Route::get( '/blogs', [BlogController::class, 'index'] );
-    Route::get( '/blog/{slug}', [BlogController::class, 'show'] );
+    Route::get('/service-categories', [ServiceController::class, 'category']);
+    Route::get('/services', [ServiceController::class, 'index']);
+    Route::get('/service/{slug}', [ServiceController::class, 'show']);
 
-    // Route::get( '/blogCategories', [BlogCategoryController::class, 'index'] );
-    Route::get( '/service-categories', [ServiceController::class, 'category'] );
-    Route::get( '/projects', [ProjectController::class, 'index'] );
-    Route::get( '/project/{slug}', [ProjectController::class, 'show'] );
-    Route::get( '/services', [ServiceController::class, 'index'] );
-    Route::get( '/service/{slug}', [ServiceController::class, 'show'] );
-    Route::get( '/products', [ProductController::class, 'index'] );
-    Route::get( '/product/{slug}', [ProductController::class, 'show'] );
-    Route::get( '/category/{slug}', [ProductCategoryController::class, 'show'] );
-    Route::get( '/product-categories', [ProductController::class, 'category'] );
-    Route::get( '/project-categories', [ProjectController::class, 'category'] );
-    // Route::get( '/productBrands', [ProductBrandController::class, 'index'] );
-    // Route::get( '/productOrigins', [ProductOriginController::class, 'index'] );
-    // Route::get( '/product-features', [ProductFeatureController::class, 'index'] );
-    // Route::get( '/product-videos', [ProductVideoController::class, 'index'] );
-    // Route::get( '/product-documents', [ProductDocumentController::class, 'index'] );
-    Route::get( '/tags', [TagController::class, 'index'] );
-    Route::get( '/social-links', [AboutUsController::class, 'social_links'] );
-    Route::get( '/messagings', [MessagingController::class, 'index'] );
-    // Route::get( '/languages', [LanguageController::class, 'index'] );
-    Route::get( '/contact-us', [AboutUsController::class, 'contact_us'] );
-    // Route::get( '/choose-us', [HomeController::class, 'chooseUs'] );
-    Route::get( '/stats', [HomeController::class, 'stat'] );
-    Route::get( '/iot', [HomeController::class, 'iot'] );
-    Route::get( '/customers', [HomeController::class, 'customer'] );
-    Route::get( '/about-us', [AboutUsController::class, 'about'] );
-    Route::get( '/branches', [AboutUsController::class, 'company'] );
-    // Route::get( '/privacy-policy', [PrivacyPolicyController::class, 'index'] );
-    Route::get( '/awards', [HomeController::class, 'award'] );
-    Route::get( '/general-settings', [AboutUsController::class, 'general_settings'] );
-    Route::get( '/missions', [AboutUsController::class, 'mission'] );
-    Route::get( '/visions', [AboutUsController::class, 'vision'] );
-    Route::get( '/brands', [AboutUsController::class, 'brands'] );
-    Route::get( '/milestones', [AboutUsController::class, 'milestones'] );
-    Route::get( '/leadership-messages', [AboutUsController::class, 'leadership_messages'] );
-    Route::get( '/sliders', [HomeController::class, 'slider'] );
-    // Route::get( '/terms-conditions', [TermsConditionController::class, 'index'] );
-    // Route::get( '/pages', [PageController::class, 'index'] );
-    // Route::get( '/page/{slug}', [PageController::class, 'show'] );
-    // Route::get( '/home-settings', [HomeController::class, 'homeSettings'] );
-    Route::post( '/contact-inquiries', [ContactInquiryController::class, 'store'] );
-    Route::post( '/quotations', [ContactInquiryController::class, 'quotation'] );
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::get('/project/{slug}', [ProjectController::class, 'show']);
+    Route::get('/project-categories', [ProjectController::class, 'category']);
 
-} );
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/product/{slug}', [ProductController::class, 'show']);
+    Route::get('/product-categories', [ProductController::class, 'category']);
+    Route::get('/category/{slug}', [ProductCategoryController::class, 'show']);
 
-Route::get( '/user', function ( Request $request ) {
+    Route::get('/tags', [TagController::class, 'index']);
+    Route::get('/social-links', [AboutUsController::class, 'social_links']);
+    Route::get('/messagings', [MessagingController::class, 'index']);
+    Route::get('/contact-us', [AboutUsController::class, 'contact_us']);
+    Route::get('/about-us', [AboutUsController::class, 'about']);
+    Route::get('/branches', [AboutUsController::class, 'company']);
+    Route::get('/general-settings', [AboutUsController::class, 'general_settings']);
+    Route::get('/missions', [AboutUsController::class, 'mission']);
+    Route::get('/visions', [AboutUsController::class, 'vision']);
+    Route::get('/brands', [AboutUsController::class, 'brands']);
+    Route::get('/milestones', [AboutUsController::class, 'milestones']);
+    Route::get('/leadership-messages', [AboutUsController::class, 'leadership_messages']);
+
+    Route::get('/stats', [HomeController::class, 'stat']);
+    Route::get('/iot', [HomeController::class, 'iot']);
+    Route::get('/customers', [HomeController::class, 'customer']);
+    Route::get('/awards', [HomeController::class, 'award']);
+    Route::get('/sliders', [HomeController::class, 'slider']);
+
+    Route::post('/contact-inquiries', [ContactInquiryController::class, 'store']);
+    Route::post('/quotations', [ContactInquiryController::class, 'quotation']);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Authenticated API  →  /api/v1/oauth/*
+|--------------------------------------------------------------------------
+*/
+Route::prefix('v1/oauth')->middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::get('/user', function (Request $request) {
     return $request->user();
-} )->middleware( 'auth:sanctum' );
+})->middleware('auth:sanctum');
